fab3e13d6dcfc54a4a19eec2c75a697351413fd9
1633595450 <1633595450@qq.com>
2026-05-14 17:51:25 +0800

    跨域问题

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+  });
+
   app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
